e7a6dbd0739bad51c7930dd0538757b4e46de177
Bob Kingstone <robert.kingstone@gmail.com>
2014-02-16 21:01:24 +0000

    attempt to add all missing files

--- LPCopiers/Controllers/HomeController.cs
+++ LPCopiers/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace LPCopiers.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Providing Cost effective copier and printer solutions";
 
             return View();
         }
