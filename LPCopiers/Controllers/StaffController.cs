d4b79137a94c02ce967ab798496794e681dde3b3
BobKingstone <robert.kingstone@gmail.com>
2014-03-23 11:29:14 +0000

    Completed Wage Calculator

--- LPCopiers/Controllers/StaffController.cs
+++ LPCopiers/Controllers/StaffController.cs
@@ -1,8 +1,4 @@
 ﻿using LPCopiers.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace LPCopiers.Controllers
@@ -21,10 +17,68 @@ namespace LPCopiers.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult WageCalculator(WageCalculator m)
         {
+            double rate = 10;   
+            var overtimeHours = 0;
+            double overtimeRate = 0;
+            double overtimePay = 0;
+
+            if(ModelState.IsValid)
+            {
+                switch (m.rate)
+                {
+                    case "Engineer":
+                        rate = 10;
+                        break;
+                    case "Senior Engineer":
+                        rate = 12;
+                        break;
+                    case "Technical Engineer":
+                        rate = 25;
+                        break;
+                    default:
+                        rate = 30;
+                        break;
+                }
+                if (m.Beng == true)
+                    rate = rate + (rate * 0.1);
+
+                if (m.hours > 40)
+                {
+                    overtimeHours = (int)m.hours - 40;
+                    m.hours = 40;
+                    overtimeRate = rate * 1.5;
+                    overtimePay = overtimeHours * overtimeRate;
+                }
+
+                m.wage = m.hours * rate;
+
+                if (overtimeHours > 0)
+                    m.wage = m.wage + overtimePay;
+                //added to reset value
+                m.hours += overtimeHours;
+
+                return RedirectToAction("Wage",m);
+            }
+
             return View();
+            
+        }
+
+        public ActionResult Wage (WageCalculator m)
+        {
+
+            if(ModelState.IsValid)
+            {  
+                
+                return View(m);
+            }
+                
+
+            return RedirectToAction("WageCalculator");
         }
 
     }
--- LPCopiers/Models/WageCalculator.cs
+++ LPCopiers/Models/WageCalculator.cs
@@ -10,13 +10,17 @@ namespace LPCopiers.Models
     public class WageCalculator
     {
         [Required]
+        [DisplayName("BEng Certificate")]
         public bool Beng { get; set; }
         [Required]
+        [DisplayName("Hours Worked")]
         [Range(1,80.0,ErrorMessage="Hours must be between 1 and 80")]
         public double hours { get; set; }
         [Required]
         [DisplayName("Job Title")]
-        public double rate { get; set; }
+        public string rate { get; set; }
+        [DisplayName("Calculated Wage")]
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public double wage { get; set; }
     }
 }
