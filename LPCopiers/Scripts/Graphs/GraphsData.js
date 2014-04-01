60126b3a0d5a2f78621374d660d5d7219b6bae52
Bob Kingstone <robert.kingstone@gmail.com>
2014-04-01 23:15:49 +0100

    updated admin area look

--- LPCopiers/Scripts/Graphs/GraphsData.js
+++ LPCopiers/Scripts/Graphs/GraphsData.js
@@ -2,18 +2,18 @@
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
 		{
-		    fillColor: "rgba(220,220,220,0.5)",
+		    fillColor: "rgba(255, 0, 0, 0.40)",
 		    strokeColor: "rgba(220,220,220,1)",
 		    pointColor: "rgba(220,220,220,1)",
 		    pointStrokeColor: "#fff",
-		    data: [65, 59, 90, 81, 56, 55, 40]
+		    data: [200, 200, 100, 300, 260, 250, 400]
 		},
 		{
 		    fillColor: "rgba(151,187,205,0.5)",
 		    strokeColor: "rgba(151,187,205,1)",
 		    pointColor: "rgba(151,187,205,1)",
 		    pointStrokeColor: "#fff",
-		    data: [28, 48, 40, 19, 96, 27, 100]
+		    data: [100, 200, 300, 400, 400, 300, 300]
 		}
     ]
 }
